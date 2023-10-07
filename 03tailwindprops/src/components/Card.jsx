922b3ce1078e8a2c5dffc54b38414b467185f34b
sagar <sagarregmi2056@gmail.com>
2023-10-07 23:35:53 +0545

    8th completed - projects tomorrow

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({username}) => {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 
-welcome to our JOB sewa
+
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
       <img
         className="mx-auto h-10 w-auto"
