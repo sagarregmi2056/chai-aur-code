0ecaf2ae4f992c185ec42b1804452a5023c2fcc1
sagar <sagarregmi2056@gmail.com>
2023-10-07 23:28:25 +0545

    props/5thdaycompleted

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+
+
+  return (
+   <>
+   <nav>home class school </nav>
+  <Card username="sagar" />
+   <Card username="nepal"/>
+   </>
+  )
+}
+
+export default App
