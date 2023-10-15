b0984108f65738735c2eeaa6ac5da7e4c239b794
sagar <sagarregmi2056@gmail.com>
2023-10-15 09:31:14 +0545

    06/currentprocess

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='bg-black font-sans text-3xl text-white'>currency converter </h1>
+        
+    </>
+  )
+}
+
+export default App
